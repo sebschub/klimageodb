caa7b71064788a1b53608bd97b837d6ff814c331
Sebastian Schubert <sebastian.schubert@geo.hu-berlin.de>
2017-12-04 17:32:15 +0100

    Add views read functions

--- R/db_read.R
+++ R/db_read.R
@@ -1,9 +1,21 @@
 #' Table reading
 #'
-#' These function retrieve the respective tables as a data frame from the
-#' database.
+#' These function retrieve the respective tables and views as a data frame from
+#' the database.
+#'
+#' Most of the listed function retrieve present tables in the database. For
+#' convinience, function of the form \code{dbReadTable_*_detail} receive the
+#' views which join different database tables together. For example,
+#' \code{dbReadTable_measurand_detail} retrieves the \code{measurand} table with
+#' all id fields replaced by the corresponding information in
+#' \code{physical_quantity}, \code{calibrated_device}, etc.
 #'
 #' @param conn Database connection.
+#' @return The functions return a data frame that contains the complete data
+#'   from the respective remote table, effectively the result of calling
+#'   \code{\link[DBI]{dbGetQuery}} with \code{SELECT * FROM <name>}. An error is
+#'   raised if the table does not exist. An empty table is returned as a data
+#'   frame with zero rows.
 #' @name readtable
 #' @seealso \code{\link[DBI]{dbReadTable}} for the general function to retrieve
 #'   complete tables. \code{\link[DBI]{dbGetQuery}} and
@@ -11,6 +23,9 @@
 #'   general SQL queries. Alternatively, you can use \code{\link[dplyr]{tbl}} of
 #'   the \code{dplyr} package to only work on part of the database.
 NULL
+
+
+# Tables ------------------------------------------------------------------
 
 #' @rdname readtable
 #' @export
@@ -94,4 +109,37 @@ dbReadTable_station_adlershof <- function(conn){
 #' @export
 dbReadTable_station_adlershof_correction <- function(conn){
   DBI::dbReadTable(conn, "station_adlershof_correction")
+}
+
+
+# Views -------------------------------------------------------------------
+
+#' @rdname readtable
+#' @export
+dbReadTable_device_model_detail <- function(conn){
+  DBI::dbReadTable(conn, "device_model_detail")
+}
+
+#' @rdname readtable
+#' @export
+dbReadTable_device_detail <- function(conn){
+  DBI::dbReadTable(conn, "device_detail")
+}
+
+#' @rdname readtable
+#' @export
+dbReadTable_calibrated_device_detail <- function(conn){
+  DBI::dbReadTable(conn, "calibrated_device_detail")
+}
+
+#' @rdname readtable
+#' @export
+dbReadTable_integration_detail <- function(conn){
+  DBI::dbReadTable(conn, "integration_detail")
+}
+
+#' @rdname readtable
+#' @export
+dbReadTable_measurand_detail <- function(conn){
+  DBI::dbReadTable(conn, "measurand_detail")
 }
